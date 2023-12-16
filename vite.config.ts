01a49d1a023d2fcb9395e72a25f919d89ef05f6c
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-12-16 23:31:55 +0900

    feat: change PWA logo

--- vite.config.ts
+++ vite.config.ts
@@ -106,8 +106,8 @@ export default () =>
           ],
         },
         manifest: {
-          name: "Outline",
-          short_name: "Outline",
+          name: "Ham's Note",
+          short_name: "Personal Note App powered by Outline",
           theme_color: "#fff",
           background_color: "#fff",
           start_url: "/",
